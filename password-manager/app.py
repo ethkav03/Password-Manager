e3fa7d83691658d0ee1580a9c36da64e8ea98b60
ethkav03 <ethkav03@gmail.com>
2023-05-19 14:40:38 +0100

    Added GUI for Login and Register pages (Not functional)

--- password-manager/app.py
+++ password-manager/app.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template
-import sqlite3
 import os
+import sqlite3
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
 from generate_password import generate
+from functions import login_required
 from pyperclip import copy
 
+# Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 con = sqlite3.connect("pwd-mngr.db")
 cur = con.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER AUTO_INCREMENT PRIMARY KEY, email TEXT NOT NULL, hash TEXT NOT NULL, verified NUMBER(0));")
+cur.execute("CREATE TABLE IF NOT EXISTS passwords (id INTEGER AUTO_INCREMENT PRIMARY KEY, user_id INTEGER NOT NULL, url TEXT NOT NULL, username TEXT NOT NULL, email TEXT NOT NULL, hash TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id));")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
